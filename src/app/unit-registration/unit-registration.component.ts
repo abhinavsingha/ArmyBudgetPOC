83be60854b12becb90f434141fe195b3e4e18616
unknown <er.nitindinkar@gmail.com>
2023-08-29 15:22:16 +0530

    user reg complt

--- src/app/unit-registration/unit-registration.component.ts
+++ src/app/unit-registration/unit-registration.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-unit-registration',
   templateUrl: './unit-registration.component.html',
   styleUrls: ['./unit-registration.component.scss']
 })
-export class UnitRegistrationComponent {
+export class UnitRegistrationComponent implements OnInit{
+  ngOnInit(): void {
+    $.getScript('assets/js/adminlte.js');
+  
+  }
 
 }
--- src/app/user-registration/user-registration.component.ts
+++ src/app/user-registration/user-registration.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-registration',
   templateUrl: './user-registration.component.html',
   styleUrls: ['./user-registration.component.scss']
 })
-export class UserRegistrationComponent {
+export class UserRegistrationComponent implements OnInit{
+ngOnInit(): void {
+  $.getScript('assets/js/adminlte.js');
 
+}
 }
